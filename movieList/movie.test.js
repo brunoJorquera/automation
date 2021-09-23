30b1269e1f1d9bf7e87b79c5e2fe50b619a73328
brunoJorquera <jorquera.b18@gmail.com>
2021-09-22 17:17:53 -0700

    All done and then some!

--- movieList/movie.test.js
+++ movieList/movie.test.js
@@ -3,6 +3,7 @@ require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
+const movie = "John Wick"
 
 beforeAll(async () => {
     await driver.get('http://localhost:5500/movieList/index.html')
@@ -14,21 +15,40 @@ afterAll(async () => {
 })
 
 
-test('Show movie page??', async () => {
+test('should add a movie', async () => {
     let searchBar = await driver.findElement(By.name('input'))
     await searchBar.sendKeys("John Wick\n")
-    let button = await driver.findElement(By.name("button"))
+    await driver.sleep(500)
+})
+
+test("Should verify text", async () => {
+    await driver.sleep(500)
+    let movieTitle = await driver.findElement(By.xpath('//ul/li/span')).getText()
+    expect(movieTitle).toEqual(movie)
+    await driver.sleep(2000)
+})
+
+test("should cross off a movie", async () => {
+    await driver.sleep(500)
+    let button = await driver.findElement(By.xpath("//ul/li/span"))
     await button.click()
 })
 
-test("Should return an interesting result", async () => {
-    await driver.get('http://google.com');
-    let searchBar = await driver.findElement(By.name('q'))
-    await searchBar.sendKeys('Never Gonna Give You Up\n')
-    let link = await driver.findElement(By.xpath(`//*[@id="kp-wp-tab-overview"]/div[1]/div/div/div/div/div/div/div[2]/h3/a/h3`))
-    await link.click()
-    await driver.sleep(10000)
-    let play = await driver.findElement(By.xpath(`//*[@id="movie_player"]/div[33]/div[2]/div[1]/button`))
-    await play.click()
-    await driver.sleep(26500)
+test("should remove the movie", async () => {
+    await driver.sleep(500)
+    let button = await driver.findElement(By.id("JohnWick"))
+    await button.click()
+    await driver.sleep(2000)
 })
+
+// test("Should return an interesting result", async () => {
+//     await driver.get('http://google.com');
+//     let searchBar = await driver.findElement(By.name('q'))
+//     await searchBar.sendKeys('Never Gonna Give You Up\n')
+//     let link = await driver.findElement(By.xpath(`//*[@id="kp-wp-tab-overview"]/div[1]/div/div/div/div/div/div/div[2]/h3/a/h3`))
+//     await link.click()
+//     await driver.sleep(10000)
+//     let play = await driver.findElement(By.xpath(`//*[@id="movie_player"]/div[33]/div[2]/div[1]/button`))
+//     await play.click()
+//     await driver.sleep(26500)
+// })
